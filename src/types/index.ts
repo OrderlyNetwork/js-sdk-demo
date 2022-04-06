31224bc5114513c7eff56c63886f835ab1ee60a7
night_lovell <andrewwwlutsenko@gmail.com>
2022-04-06 23:00:22 +0300

    Change testnet to local

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,18 @@
-import { IWallet } from './types';
+import { Contract, Near, WalletConnection } from 'near-api-js';
 
-export type { IWallet };
+export interface IChildren {
+	children: React.ReactNode;
+}
+export interface IWallet {
+	readonly wallet_address?: string;
+	wallet_name?: string;
+	total_balance?: string;
+	accessible_balance?: string;
+	wallet_currency?: string;
+	contract?: Contract;
+	walletConnection?: WalletConnection;
+	currentUser?: any;
+	nearConfig?: object;
+	near?: Near;
+	isLoading: boolean;
+}
