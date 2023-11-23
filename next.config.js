93691a7475f297a999af046597068b8c01f4fdc1
maogege <hemaojun@foxmail.com>
2023-11-23 09:42:01 +0800

    integration orderly SDK

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+reactStrictMode: true,
+    webpack:(config,{isServer})=>{
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+              fs: false,
+            };
+          }
+        return config
+    }
+}
+
+module.exports = nextConfig
