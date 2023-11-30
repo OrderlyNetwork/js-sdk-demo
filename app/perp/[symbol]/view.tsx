0ab92606177ce3bfb36bd01fac1f718bcd5ffb51
Mark <mark@orderly.network>
2023-11-30 22:33:10 +0800

    config default support connect wallet type

--- app/perp/[symbol]/view.tsx
+++ app/perp/[symbol]/view.tsx
@@ -1,6 +1,9 @@
 import { ConnectorProvider } from '@orderly.network/web3-onboard';
 import { OrderlyAppProvider, TradingPage } from '@orderly.network/react';
-import { useState } from 'react';
+import injectedModule from "@web3-onboard/injected-wallets";
+import ledgerModule from '@web3-onboard/ledger'
+import trezorModule from '@web3-onboard/trezor'
+import walletConnectModule from '@web3-onboard/walletconnect';
 
 interface Props {
 	onSymbolChange: (symbol: string) => void;
@@ -15,8 +18,31 @@ const tradingViewConfig: any = {
 const View = (props: Props) => {
 
 	const networkId = localStorage.getItem('orderly-networkId') ?? 'testnet';
+
+	const wcV2InitOptions = {
+		version: 2,
+		projectId: '93dba83e8d9915dc6a65ffd3ecfd19fd',
+		requiredChains: [42161],
+		optionalChains: [421613, 42161],
+		dappUrl: window.location.host,
+	};
+
+	const ledgerInitOptions = {
+		projectId: '93dba83e8d9915dc6a65ffd3ecfd19fd',
+	};
+	const walletConnect = walletConnectModule(wcV2InitOptions);
+	const ledger = ledgerModule(ledgerInitOptions);
+
+
+	const trezor = trezorModule({
+		email: '<EMAIL_CONTACT>',
+		appUrl: '<APP_URL>'
+	  })
+
 	return (
-		<ConnectorProvider>
+		<ConnectorProvider options={{
+			wallets: [injectedModule(), walletConnect, ledger, trezor],
+		}}>
 			<OrderlyAppProvider
 				networkId={networkId}
 				brokerId="woofi_pro"
