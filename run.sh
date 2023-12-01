43d29f8ab985e181b6a72d5f2f4524d409fea1e1
Mark <mark@orderly.network>
2023-12-01 10:41:41 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 pnpm install @orderly.network/react@internal
 pnpm install @orderly.network/web3-onboard@internal
 pnpm dev
+
+pnpm i @web3-onboard/trezor @web3-onboard/walletconnect @web3-onboard/ledger @web3-onboard/injected-wallets
