797dce496d7f02114e55605647ea13482f166f29
Mark <mark@orderly.network>
2023-12-04 20:17:06 +0800

    update css and close scale effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,22 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Orderly SDK Demo',
-	description: 'Orderly SDK Demo',
+	description: 'Orderly SDK Demo'
 };
+
+export const viewport = {
+	minimumScale: 1.0,
+	maximumScale: 1.0,
+	userScalable: false,
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-      <head>
-        <link rel='icon' type='image/png' sizes='16x16' href='/apple-touch-icon.png' />
-        <link rel='apple-touch-icon' type='image/png' sizes='16x16' href='/apple-touch-icon.png' />
-
-      </head>
+			<head>
+				<link rel='icon' type='image/png' sizes='16x16' href='/apple-touch-icon.png' />
+				<link rel='apple-touch-icon' type='image/png' sizes='16x16' href='/apple-touch-icon.png' />
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
