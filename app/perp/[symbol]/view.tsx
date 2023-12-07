cae19109494c91b5eccfb60fa4c61cd42411982c
Mark <mark@orderly.network>
2023-12-07 18:45:39 +0800

    update connect wallet bottom sheet app icon

--- app/perp/[symbol]/view.tsx
+++ app/perp/[symbol]/view.tsx
@@ -67,6 +67,25 @@ const View = (props: Props) => {
 				ledger, 
 				// trezor,
 			],
+			appMetadata: {
+				name: "Orderly",
+				icon: '/OrderlyLogo.png',
+				description: "Orderly",
+				recommendedInjectedWallets: [
+				  { name: "Coinbase", url: "https://wallet.coinbase.com/" },
+				  { name: "MetaMask", url: "https://metamask.io" },
+				  { name: "Trezor", url: "https://trezor.io/" },
+				  { name: "Walletconnect", url: "https://walletconnect.com/"},
+				  { name: "Ledger", url: "https://www.ledger.com/"}
+				],
+				agreement: {
+				  version: "1.0.0",
+				  termsUrl: "https://www.blocknative.com/terms-conditions",
+				  privacyUrl: "https://www.blocknative.com/privacy-policy",
+				},
+				gettingStartedGuide: "https://blocknative.com",
+				explore: "https://blocknative.com",
+			  }
 		}}>
 			<OrderlyAppProvider
 				networkId={networkId}
