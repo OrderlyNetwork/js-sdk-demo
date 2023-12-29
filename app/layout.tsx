eb402281be48b307127d1184d65b75bbbc1b4335
Jame Zhan <jame@orderly.network>
2023-12-29 16:31:09 +0800

    remove next default class

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 // export const metadata: Metadata = {
 // 	title: 'Orderly SDK Demo',
@@ -13,27 +9,26 @@ export const viewport = {
 	minimumScale: 1.0,
 	maximumScale: 1.0,
 	userScalable: false,
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<head>
-				<link rel='icon' type='image/png' href='/apple-touch-icon.png' />
-				<link rel='apple-touch-icon' type='image/png' sizes='16x16' href='/apple-touch-icon.png' />
+				<link rel="icon" type="image/png" href="/apple-touch-icon.png" />
+				<link rel="apple-touch-icon" type="image/png" sizes="16x16" href="/apple-touch-icon.png" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-					rel="stylesheet"
-				/>
-
-
+				<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700&family=Noto+Sans:wght@500'600'700&family=Oswald:wght@200;300;400&display=swap" rel="stylesheet" />			</head>
-				<title id="orderly_sdk_demo_title_key">Orderly SDK Demo</title>
-				<meta name='description' content='Orderly SDK Demo'/>
-			<body className={inter.className}>{children}</body>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700&family=Noto+Sans:wght@500'600'700&family=Oswald:wght@200;300;400&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<title id="orderly_sdk_demo_title_key">Orderly SDK Demo</title>
+			<meta name="description" content="Orderly SDK Demo" />
+			<body>{children}</body>
 		</html>
 	);
 }
