188cbbc212aa7e44360d8b9ea8cc0a4b7f722461
Mark <mark@orderly.network>
2024-01-15 11:17:21 +0800

    update get font from local files

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,14 +17,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<head>
 				<link rel="icon" type="image/png" href="/apple-touch-icon.png" />
 				<link rel="apple-touch-icon" type="image/png" sizes="16x16" href="/apple-touch-icon.png" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				{/* <link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700&family=Noto+Sans:wght@500'600'700&family=Oswald:wght@200;300;400&display=swap"
+					href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700&family=Noto+Sans:wght@500;600;700&family=Oswald:wght@200;300;400;500;600;700;800&display=swap"
 					rel="stylesheet"
-				/>
+				/> */}
 			</head>
 			<title id="orderly_sdk_demo_title_key">Orderly SDK Demo</title>
 			<meta name="description" content="Orderly SDK Demo" />
