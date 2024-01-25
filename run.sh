520c2d3f245eae51fe5b27c9e0a3fc4764b3033b
Mark <mark@orderly.network>
2024-01-25 12:02:03 +0800

    update SDK Version

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 pnpm i @orderly.network/react@internal @orderly.network/web3-onboard@internal
 pnpm i @orderly.network/react@demo @orderly.network/web3-onboard@demo
-pnpm i @orderly.network/react@alpha @orderly.network/web3-onboard@alpha
+pnpm i @orderly.network/react@alpha @orderly.network/web3-onboard@alpha @orderly.network/core@alpha
 pnpm dev
 
 pnpm i @web3-onboard/trezor @web3-onboard/walletconnect @web3-onboard/ledger @web3-onboard/injected-wallets
