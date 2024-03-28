d07d28b8dccd21216b3ff4747dad1c0e24f6fa42
Mark <mark@orderly.network>
2024-03-28 10:00:10 +0800

    tp sl

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ pnpm dev
 pnpm i @web3-onboard/trezor @web3-onboard/walletconnect @web3-onboard/ledger @web3-onboard/injected-wallets
 
 
-pnpm i @orderly.network/react@internal @orderly.network/web3-onboard@internal @orderly.network/core@internal
+pnpm i @orderly.network/react@internal @orderly.network/web3-onboard@internal @orderly.network/core@internal @orderly.network/hooks@internal
 pnpm dev
 
 #编译：
