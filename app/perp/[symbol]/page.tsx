10190b7a15ef51a5fee2b93a777ac5b3d06e3cf3
Jame Zhan <jame@orderly.network>
2024-04-11 23:34:45 +0800

    add portfolio page

--- app/perp/[symbol]/page.tsx
+++ app/perp/[symbol]/page.tsx
@@ -1,26 +1,20 @@
 'use client';
-
-import { ORDERLY_SDK_DEMO_TITLE_KEY } from '@/app/config';
-import '@orderly.network/react/dist/styles.css';
+import { useCallback } from 'react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
-import { useCallback } from 'react';
-
-// export function
+import { ORDERLY_SDK_DEMO_TITLE_KEY } from '@/app/config';
+import { _orderlySymbolKey } from '@/app/constant';
+import '@orderly.network/react/dist/styles.css';
 
 const MainView = dynamic(() => import('./view'), { ssr: false });
 
-const _orderlySymbolKey = 'orderly-sdk-demo-symbol';
-
 export default function PerpPage({ params }: { params: { slug: string } }) {
 	const router = useRouter();
-	
 
 	let symbol = params.slug;
 	if (symbol === undefined) {
-		symbol = localStorage.getItem(_orderlySymbolKey) ?? 'PERP_ETH_USDC';
+		symbol = localStorage?.getItem(_orderlySymbolKey) ?? 'PERP_ETH_USDC';
 	}
-
 
 	const updateTitle = useCallback((title) => {
 		var titleElement = document.getElementById(ORDERLY_SDK_DEMO_TITLE_KEY);
@@ -29,15 +23,16 @@ export default function PerpPage({ params }: { params: { slug: string } }) {
 		}
 	}, []);
 
-	return <MainView
-		symbol={symbol}
-		onSymbolChange={(symbol) => {
-			console.log('update symbol', symbol);
-			localStorage.setItem(_orderlySymbolKey, symbol.symbol);
-			router.push(`/perp/${symbol.symbol}`);
+	return (
+		<MainView
+			symbol={symbol}
+			onSymbolChange={(symbol) => {
+				console.log('update symbol', symbol);
+				localStorage.setItem(_orderlySymbolKey, symbol.symbol);
+				router.push(`/perp/${symbol.symbol}`);
 
-			updateTitle(symbol.symbol);
-
-		}}
-	/>;
+				updateTitle(symbol.symbol);
+			}}
+		/>
+	);
 }
--- app/perp/[symbol]/view.tsx
+++ app/perp/[symbol]/view.tsx
@@ -1,66 +1,19 @@
-import { useCallback } from 'react';
-import { ConnectorProvider } from '@orderly.network/web3-onboard';
-import { OrderlyAppProvider, TradingPage } from '@orderly.network/react';
+import { TradingPage } from '@orderly.network/react';
 import { OrderlyConfig } from '@/app/config';
-import { CustomConfigStore, ENV_NAME } from './CustomConfigStore';
+import OrderlyContainer from '@/app/common/OrderlyContainer';
 
 interface Props {
 	onSymbolChange: (symbol: any) => void;
 	symbol: string;
 }
 
-export type NetworkId = 'testnet' | 'mainnet';
-
-const HostEnvMap: Record<string, ENV_NAME> = {
-	'dev-sdk-demo.orderly.network': 'dev',
-	'qa-sdk-demo.orderly.network': 'qa',
-	'sdk-demo-iap.orderly.network': 'staging',
-	'localhost': 'staging',
-};
-
 const View = (props: Props) => {
-	const networkId = (localStorage.getItem('orderly-networkId') ?? 'mainnet') as NetworkId;
-
-	const { onboard, app, tradingViewConfig } = OrderlyConfig();
-
-	const onChainChanged = useCallback(
-		(chainId, isTestnet) => {
-			// console.log('chain changed', chainId, isTestnet);
-			localStorage.setItem('orderly-networkId', isTestnet ? 'testnet' : 'mainnet');
-			// realod page
-			setTimeout(() => {
-				window.location.reload();
-			}, 100);
-		},
-		[props.symbol],
-	);
-
-	const env = networkId === 'mainnet' ? 'prod' : HostEnvMap[window.location.hostname] || 'staging';
-
-	const configStore = new CustomConfigStore({ networkId, env });
+	const { tradingViewConfig } = OrderlyConfig();
 
 	return (
-		<ConnectorProvider options={onboard as any}>
-			<OrderlyAppProvider
-				configStore={configStore}
-				networkId={networkId}
-				brokerId={app.brokerId}
-				brokerName={app.brokerName}
-				appIcons={app.appIcons}
-				onChainChanged={onChainChanged}
-				footerStatusBarProps={app.footerStatusBarProps}
-				shareOptions={app.shareOptions}
-				// onBoundRefCode={() => {}}
-				// onClickReferral={() => {}}
-				// saveRefCode={(localStorage.getItem("enable_save_ref_code") || "0") === "1"}
-			>
-				<TradingPage
-					symbol={props.symbol}
-					tradingViewConfig={tradingViewConfig}
-					onSymbolChange={props.onSymbolChange}
-				/>
-			</OrderlyAppProvider>
-		</ConnectorProvider>
+		<OrderlyContainer>
+			<TradingPage symbol={props.symbol} tradingViewConfig={tradingViewConfig} onSymbolChange={props.onSymbolChange} />
+		</OrderlyContainer>
 	);
 };
 
