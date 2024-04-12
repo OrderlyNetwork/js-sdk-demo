6cf1feeeac69d009aa5a90a6fbf7fdc91936e58e
Jame Zhan <jame@orderly.network>
2024-04-12 13:12:53 +0800

    add tailwind prefix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  prefix: "orderly-",
   theme: {
     extend: {
       backgroundImage: {
