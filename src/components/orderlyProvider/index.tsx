7896a54cc641336341bee510e07bd939823ac451
Mark <mark@orderly.network>
2024-11-28 20:33:13 +0800

    update

--- src/components/orderlyProvider/index.tsx
+++ src/components/orderlyProvider/index.tsx
@@ -3,56 +3,10 @@ import React, { FC, ReactNode } from "react";
 import { WalletConnectorProvider } from "@orderly.network/wallet-connector";
 import { OrderlyAppProvider } from "@orderly.network/react-app";
 import config from "@/config";
-import {
-  createDefaultAddressSelector,
-  createDefaultAuthorizationResultCache,
-  SolanaMobileWalletAdapter,
-} from "@solana-mobile/wallet-adapter-mobile";
-import {
-  Adapter,
-  WalletAdapterNetwork,
-  WalletError,
-  WalletNotReadyError,
-} from "@solana/wallet-adapter-base";
-import {
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-
-
 
 const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
-
-  const network = WalletAdapterNetwork.Devnet;
-
-const mobileWalletNotFoundHanlder = (adapter: SolanaMobileWalletAdapter) => {
-  console.log("-- mobile wallet adapter", adapter);
-
-  return Promise.reject(new WalletNotReadyError("wallet not ready"));
-};
-
-const handleSolanaError = (error: WalletError, adapter?: Adapter) => {
-  console.log("-- solanan error", error);
-  console.log("-- solana adapter", adapter);
-};
-
-const wallets = [
-  new PhantomWalletAdapter(),
-  new SolflareWalletAdapter(),
-  new SolanaMobileWalletAdapter({
-    addressSelector: createDefaultAddressSelector(),
-    appIdentity: {
-      uri: `${location.protocol}//${location.host}`,
-    },
-    authorizationResultCache: createDefaultAuthorizationResultCache(),
-    chain: network,
-    onWalletNotFound: mobileWalletNotFoundHanlder,
-  }),
-];
   return (
-    <WalletConnectorProvider
-      solanaInitial={{ wallets: wallets, onError: handleSolanaError }}
-    >
+    <WalletConnectorProvider>
       <OrderlyAppProvider
         brokerId="demo"
         brokerName="Orderly"
