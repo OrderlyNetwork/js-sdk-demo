5786084526d4dc7cd304384c45c4873bac02d03c
mark <mark@orderly.network>
2024-12-04 18:52:31 +0800

    support changed networkId

--- src/components/orderlyProvider/index.tsx
+++ src/components/orderlyProvider/index.tsx
@@ -2,16 +2,31 @@
 import React, { FC, ReactNode } from "react";
 import { WalletConnectorProvider } from "@orderly.network/wallet-connector";
 import { OrderlyAppProvider } from "@orderly.network/react-app";
+import { useLocalStorage } from "@orderly.network/hooks";
 import config from "@/config";
 
 const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
+  const [networkId, setNetworkId] = useLocalStorage(
+    "dmm-local-storage-network-id",
+    "mainnet"
+  );
+
   return (
     <WalletConnectorProvider>
       <OrderlyAppProvider
         brokerId="orderly"
         brokerName="Orderly"
-        networkId="mainnet"
+        networkId={networkId}
         appIcons={config.orderlyAppProvider.appIcons}
+        onChainChanged={(
+          chainId: number,
+          state: {
+            isTestnet: boolean;
+            isWalletConnected: boolean;
+          }
+        ) => {
+          setNetworkId(state.isTestnet ? "testnet" : "mainnet");
+        }}
       >
         {props.children}
       </OrderlyAppProvider>
