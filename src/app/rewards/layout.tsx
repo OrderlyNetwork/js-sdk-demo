0aa8dd110026d1c79139b76a854e1b1896b9750c
Jame Zhan <jame@orderly.network>
2025-04-20 01:04:27 +0800

    add rewards page

---
+++ src/app/rewards/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { ReactNode } from "react";
+import { TradingRewardsLayoutWidget } from "@orderly.network/trading-rewards";
+import { useOrderlyConfig } from "@/hooks/useOrderlyConfig";
+import { useNav } from "@/hooks/useNav";
+import { PathEnum } from "@/constant";
+import { usePathWithoutLang } from "@/hooks/usePathWithoutLang";
+
+export default function TradingRewardsLayout(props: { children: ReactNode }) {
+  const config = useOrderlyConfig();
+  const path = usePathWithoutLang();
+
+  const { onRouteChange } = useNav();
+
+  return (
+    <TradingRewardsLayoutWidget
+      footerProps={config.scaffold.footerProps}
+      mainNavProps={{
+        ...config.scaffold.mainNavProps,
+        initialMenu: PathEnum.Rewards,
+      }}
+      routerAdapter={{
+        onRouteChange,
+      }}
+      leftSideProps={{
+        current: path,
+      }}
+    >
+      {props.children}
+    </TradingRewardsLayoutWidget>
+  );
+}
