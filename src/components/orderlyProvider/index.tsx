b961abc525a4008117d325cc2fd13de5a3174e88
tony <tony@woo.network>
2025-04-25 16:18:18 +0800

    use wallet-connector-privy

--- src/components/orderlyProvider/index.tsx
+++ src/components/orderlyProvider/index.tsx
@@ -1,16 +1,13 @@
 "use client";
 import React, { FC, ReactNode, useEffect, useMemo, useState } from "react";
-import { WalletConnectorProvider } from "@orderly.network/wallet-connector";
 import { OrderlyAppProvider } from "@orderly.network/react-app";
 import { useLocalStorage } from "@orderly.network/hooks";
-import walletConnectModule, {
-  type WalletConnectOptions,
-} from "@web3-onboard/walletconnect";
-import injectedModule from "@web3-onboard/injected-wallets";
-import ledgerModule, { LedgerOptionsWCv2 } from "@web3-onboard/ledger";
-import binanceModule from "@binance/w3w-blocknative-connector";
-import trezorModule from "@web3-onboard/trezor";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { Network, WalletConnectorPrivyProvider, wagmiConnectors } from '@orderly.network/wallet-connector-privy';
+import {
+  Adapter,
+  WalletAdapterNetwork,
+  WalletError,
+} from "@solana/wallet-adapter-base";
 import {
   LedgerWalletAdapter,
   PhantomWalletAdapter,
@@ -25,6 +22,11 @@ import {
 } from "@orderly.network/i18n";
 import { usePathWithoutLang } from "@/hooks/usePathWithoutLang";
 
+const getPrivyId = () => {
+  // dev privy id
+  return 'cm86zfufk01n2ojo83s2becsr';
+};
+
 const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
   const config = useOrderlyConfig();
   const path = usePathWithoutLang();
@@ -33,49 +35,10 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
     "dmm-local-storage-network-id",
     "mainnet"
   );
-  const [wcOptions, setWcOptions] = useState();
 
-  useEffect(() => {
-    const wcV2InitOptions: WalletConnectOptions = {
-      version: 2,
-      projectId: "93dba83e8d9915dc6a65ffd3ecfd19fd",
-      requiredChains: [42161],
-      optionalChains: [42161, 10, 1, 56, 43114, 8453, 137, 59144, 421614, 420],
-      dappUrl: window.location.host,
-    };
 
-    setWcOptions(wcV2InitOptions);
-  }, []);
 
-  const walletConnect = useMemo(() => {
-    if (!wcOptions) return undefined;
-    return walletConnectModule(wcOptions ?? {});
-  }, [wcOptions]);
 
-  const trezor = trezorModule({
-    email: "<EMAIL_CONTACT>",
-    appUrl: "<APP_URL>",
-  });
-  const injected = injectedModule();
-  const ledgerInitOptions: LedgerOptionsWCv2 = {
-    walletConnectVersion: 2,
-    projectId: "93dba83e8d9915dc6a65ffd3ecfd19fd",
-  };
-  const ledger = ledgerModule(ledgerInitOptions);
-
-  const binance = binanceModule({ options: { lng: "en" } });
-
-  if (!walletConnect) {
-    return <></>;
-  }
-
-  const wallets = [
-    injected,
-    trezor,
-    ledger,
-    binance,
-    walletConnect /* bitgetWallet */,
-  ];
 
   const solWallets = [
     new PhantomWalletAdapter(),
@@ -101,20 +64,42 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
       onLanguageChanged={onLanguageChanged}
       backend={{ loadPath }}
     >
-      <WalletConnectorProvider
-        evmInitial={{
-          options: {
-            wallets,
+      <WalletConnectorPrivyProvider
+        termsOfUse={"https://learn.woo.org/legal/terms-of-use"}
+        network={networkId}
+        solanaConfig={{
+          wallets: solWallets as Adapter[],
+          onError: (error: WalletError, adapter?: Adapter) => {
+            console.log("solana wallet error", error, adapter);
+            console.error(error);
           },
         }}
-        solanaInitial={{
-          wallets: solWallets,
-          network:
-            networkId === "testnet"
-              ? WalletAdapterNetwork.Devnet
-              : WalletAdapterNetwork.Mainnet,
-          mainnetRpc:
-            "https://svc.blockdaemon.com/solana/mainnet/native?apiKey=zpka_a8c397bf72c241bea3d5562b39797cfd_3acf7e3d",
+        wagmiConfig={{
+          connectors: [
+            wagmiConnectors.injected(),
+            wagmiConnectors.walletConnect({
+              projectId: "93dba83e8d9915dc6a65ffd3ecfd19fd",
+              showQrModal: true,
+              storageOptions: {},
+              metadata: {
+                name: "Orderly",
+                description: "Orderly",
+                url: "https://orderly.network",
+                icons: ["/orderly-logo.svg"],
+              },
+            }),
+          ],
+        }}
+        privyConfig={{
+          appid: getPrivyId(),
+          config: {
+            loginMethods: ["email", "google", "twitter"],
+            appearance: {
+              theme: "dark",
+              accentColor: "#181C23",
+              logo: "/orderly-logo.svg",
+            },
+          },
         }}
       >
         <OrderlyAppProvider
@@ -129,14 +114,7 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
               isWalletConnected: boolean;
             }
           ) => {
-            // const curChainId = curChain.current;
-            // const nextChainId = chainId;
-            // curChain.current = nextChainId;
             const nextState = state.isTestnet ? "testnet" : "mainnet";
-            // console.log("nextState", {
-            //   networkId,
-            //   nextState,
-            // });
             setNetworkId(nextState);
             if (networkId !== nextState) {
               window.location.reload();
@@ -145,7 +123,7 @@ const OrderlyProvider: FC<{ children: ReactNode }> = (props) => {
         >
           {props.children}
         </OrderlyAppProvider>
-      </WalletConnectorProvider>
+      </WalletConnectorPrivyProvider>
     </LocaleProvider>
   );
 };
