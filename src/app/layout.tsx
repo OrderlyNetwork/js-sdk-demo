cd4f98125d287bca767ee133039c5019d0485991
Jame Zhan <jame@orderly.network>
2025-04-28 10:10:43 +0800

    update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { AppProvider } from "@/components/orderlyProvider/appProvider";
 
 export const metadata: Metadata = {
-  title: "Orderly SDK Demo",
-  description: "Orderly SDK Demo",
+  title: "Orderly SDK DMM",
+  description: "Orderly SDK DMM",
 };
 
 export default function RootLayout({
