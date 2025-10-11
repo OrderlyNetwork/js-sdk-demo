8aedbb8d4c6210cd736d08ca1ed721ab326bc329
Jame Zhan <jame@orderly.network>
2025-10-11 17:00:15 +0800

    add maxSubAccountCount config

---
+++ src/components/orderlyConfig/widgetConfigs.tsx
@@ -0,0 +1,7 @@
+import type { OrderlyAppProviderProps } from "@orderly.network/react-app";
+
+export const widgetConfigs: OrderlyAppProviderProps["widgetConfigs"] = {
+  subAccount: {
+    maxSubAccountCount: 9999,
+  },
+};
