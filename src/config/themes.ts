5388eb3cf58d34a8dd07e9faf12a37c40f5c8841
Jame Zhan <jame@orderly.network>
2026-03-02 11:46:04 +0800

    update theme config

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -1,4 +1,5 @@
 import { LIGHT_THEME_CSS_VARS, ThemeConfig } from "@orderly.network/ui";
+import { CUSTOM_DARK_THEME_CSS_VARS } from "@/theme";
 
 export const themes: ThemeConfig[] = [
   {
@@ -11,5 +12,11 @@ export const themes: ThemeConfig[] = [
     displayName: "Light",
     mode: "light",
     cssVars: LIGHT_THEME_CSS_VARS,
+  },
+  {
+    id: "custom-dark",
+    displayName: "Dark (Custom)",
+    mode: "dark",
+    cssVars: CUSTOM_DARK_THEME_CSS_VARS,
   },
 ];
